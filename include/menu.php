a68a10939378ef02ef6399670258f0f2ca12c20f
laubezque <laurabezaq@gmail.com>
2019-05-23 20:52:42 +0200

    menu

--- include/menu.php
+++ include/menu.php
@@ -1,23 +1,18 @@
-
-
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
 	<meta charset="utf-8">
  	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>menu</title>
-   <link rel="stylesheet" type="text/css" href="1.page.css">
    <link rel="stylesheet" type="text/css" href="css/menu.css">
 
 </head>
 
-<!--<body>
-	<nav>
-		<ul>
-			<li><a href="sobre_nosotros.php">Sobre nosotros</a></li>
-	  		<li><a href="citas.php">Citas</a></li>
-			<li><a href="contacto.php">Contacto</a></li>
+<body>
+	<ul>
+		<li><a href="sobre_nosotros.php">Sobre nosotros</a></li>
+	  	<li><a href="citas.php">Citas</a></li>
+		<li><a href="contacto.php">Contacto</a></li>
 
 
 		
@@ -25,7 +20,7 @@
 		<li><?php if (isset($_SESSION['login'])) {	?>
 				<a href="logout.php">Desconectar</a>
 			<? }else{ ?>
-				<a href="login.php">Iniciar Sesión</a>
+				<a href="login.php">Iniciar Sesiï¿½n</a>
 			<?php } ?>
 		</li>
 		
@@ -33,18 +28,8 @@
 			<a href="javascript:void(0);" onclick="myToggleMenu()">&#9776;</a>
 		</li>	
 	</ul>
-</nav>
-</body>
--->
-<body>
-    <ul>
-        <li><a href="#">home</a></li>
-        <li><a href="#">about</a></li>
-        <li><a href="#">services</a></li>
-        <li><a href="#">team</a></li>
-        <li><a href="#">portfolio</a></li>
-        <li><a href="#">contact</a></li>
-    </ul>
 
 </body>
+
 </html>
+			
