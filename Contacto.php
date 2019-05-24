fe60ea9a6b922eb8094a8a562f4ac5a6a3027bbe
Wylas <enrique_puerto97@hotmail.es>
2019-05-24 12:42:24 +0200

    css contacto

--- Contacto.php
+++ Contacto.php
@@ -6,39 +6,22 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<<<<<<< HEAD
-<style>
-	.container {
-  height: 35px;
-  
- 
-	}
 
-	.vertical-center {
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
-	right: -90px;
-	}
-	
-</style>
-=======
-
->>>>>>> 3db62fdbd1c408b6916e1ca10f7698b05aa9b543
   <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <!-- Hay que indicar el fichero externo de estilos -->
   <link rel="stylesheet" type="text/css" href="css/contacto.css" />
 	<script type="text/javascript" src="./js/boton.js"></script>
-  <title>contacto</title>
+	<title>contacto</title>
+	<?php
+		include_once("head.php");
+	?>
 </head>
 
 <body>
 <?php	
-	include_once("include/cabecera.php"); 
-	include_once("include/menu.php");
+	include_once("cabecera.php"); 
+	include_once("menu.php");
 ?>
 <main>
 
@@ -99,19 +82,10 @@
 		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1844.269772337436!2d-5.798117441185871!3d38.97698158831014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1459999beb0727%3A0x8954b11b82f5a1b4!2sHuellas+Clinica+Veterinaria!5e0!3m2!1ses!2ses!4v1558458729690!5m2!1ses!2ses" width="600" height="550" frameborder="0" style="border:0" allowfullscreen></iframe>
 	</div>
 </div>
-<<<<<<< HEAD
-
-
-</ul>
-
-
-
-=======
->>>>>>> 3db62fdbd1c408b6916e1ca10f7698b05aa9b543
 </main>
 
 <?php	
-	include_once("include/pie.php");
+	include_once("pie.php");
 ?>		
 </body>
 </html>
