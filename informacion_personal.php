643ce32359b88d12f4ee1b32b9197514bec3ba27
Wylas <enrique_puerto97@hotmail.es>
2019-05-31 18:56:46 +0200

    modificar cliente

--- informacion_personal.php
+++ informacion_personal.php
@@ -18,7 +18,7 @@
 	<meta charset="utf-8">
  	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <!-- Hay que indicar el fichero externo de estilos -->
-  <link rel="stylesheet" type="text/css" href="css/sobre_nosotros.css" />
+  <link rel="stylesheet" type="text/css" href="css/informacion_personal.css" />
 	
   <title>Sobre nosotros</title>
   <?php include_once("head.php");?>
@@ -36,14 +36,14 @@ include_once("cabecera.php");
 
 <div class="p">
 	<div class = "p1">
-        <p>DNI: <?php echo $cliente['DNI'];?></p>
+        <p>DNI: <?php echo $cliente['DNI'];?> </p>
         <p>Fecha de nacimiento: <?php echo $cliente['FECHANAC'];?></p>
         <p>Numero de teléfono: <?php echo $cliente['NUMEROTELEFONO'];?></p>
         <p>Dirección: <?php echo $cliente['DIRECCION'];?></p>
         <p>Email: <?php echo $cliente['EMAIL'];?></p>
         <p>Nombre: <?php echo $cliente['NOMBRE'];?></p>
         <p>Apellidos: <?php echo $cliente['APELLIDOS'];?></p>
-
+        <input class="butn" type="submit" value="Modificar" />
 	</div>
 	<div class = "p2">
 	</div>
