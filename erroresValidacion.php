ea8942d57447b8482c9bcecdfd190e5b2eb18da0
AntonioJesusGarciaNieto <AntonioJesusGarciaNieto@users.noreply.github.com>
2019-08-06 12:11:48 +0200

    CSS errores

--- erroresValidacion.php
+++ erroresValidacion.php
@@ -31,6 +31,7 @@
   <link rel="stylesheet" type="text/css" href="css/form_alta_cita.css" />
   <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
   <!--<script src="https://code.jquery.com/jquery-3.1.1.min.js" type="text/javascript"></script>-->
+  <link href="https://fonts.googleapis.com/css?family=ABeeZee&amp;display=swap" rel="stylesheet">
   <script src="js/validacion_cliente_alta_usuario.js" type="text/javascript"></script>
   <title>Gestión de Clinica Veterinaria: Alta de Cita</title>
   <?php include_once("head_staff.php");?>
@@ -52,11 +53,12 @@
 		// Mostrar los erroes de validación (Si los hay)
 		if (isset($errores) && count($errores)>0) { 
 	    	echo "<div id=\"div_errores\" class=\"error\" style=\"margin-top:20%\">";
-			echo "<h4> Errores en el formulario:</h4>";
+			echo "<div id =\"Obj1\" class = \"error\" style=\"font-family: 'ABeeZee', sans-serif;\">><h1> Errores en el formulario:</h1>";echo "</div>";
     		foreach($errores as $error){
     			echo $error;
 			} 
-    		echo "</div>";
+			
+			echo "</div>";
   		}
 	?>
 	
